b6fc1e3b444733cb9cc821c436f04df555324906
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2018-12-21 14:35:15 +0530

    completed

--- regfile.v
+++ regfile.v
@@ -7,20 +7,47 @@ module testbench();
     // Initialize all variables
     initial begin        
         $display ("time\t clk reset ctrl in out1 out2");	
-        $monitor ("%g\t  %d    %d   %d   %d  %d  %d", $time, clk, reset, ctrl, IN, OUT1, OUT2);	
-        clk = 1;       // initial value of clock
+        $monitor ("%g\t  %d    %d   %d   %d  %d  %d", $time, clk, reset, ctrl, IN, OUT1, OUT2);
+
+        clk = 0;       // initial value of clock
         reset = 0;       // initial value of reset
-        ctrl = 0;
+        ctrl = 1;
         
-        #5 @(negedge clk) ctrl = 1;
-        #5 @(negedge clk) IN = 22;
+        //write
+        #5 ctrl = 0;
         #5 @(negedge clk) INaddr = 2;
+        #5 @(negedge clk) IN = 200;
+
+        #5 @(negedge clk) INaddr = 4;
+        #5 @(negedge clk) IN = 100;
 
 
+        $dumpfile("R.vcd");
+	      $dumpvars(0, regF);
 
-        #10 @(posedge clk) ctrl=0;
-        #10 @(posedge clk) OUT1addr = 2;
-        #10 OUT2addr = 4;
+        //read 
+        #5 ctrl=1;
+        #5 @(posedge clk) OUT1addr = 2;
+        #5 @(posedge clk) OUT2addr = 4;
+        
+        //reset
+        #5 reset=1;
+
+        #5 @(posedge clk) OUT1addr = 2;
+        #5 @(posedge clk) OUT2addr = 4;
+
+        // off reset
+        #5 reset=0;
+
+        //write
+        #5 ctrl=0;
+        #5 @(negedge clk) INaddr = 2;
+        #5 @(negedge clk) IN = 33;
+        
+        //read
+        #5 ctrl=1;
+        #5 @(posedge clk) OUT1addr = 2;
+        #5 @(posedge clk) OUT2addr = 4;
 
         #5 $finish;      // Terminate simulation
     end
@@ -48,66 +75,82 @@ module regfile8x8a(IN, OUT1, OUT2, INaddr, OUT1addr, OUT2addr, CLK, RESET, CTRL)
 
     reg [7:0] 	 register0, register1, register2, register3, register4, register5, register6, register7;
     
+    //RESET
+    always @(*)
+    if (RESET==1)
+      begin
+        register0<=0;
+        register1<=0;
+        register2<=0;
+        register3<=0;
+        register4<=0;
+        register5<=0;
+        register6<=0;
+        register7<=0;
+      end
+        
+      
     // Write functionality 
     always @(negedge CLK ) begin
       if (CTRL==0)  begin
         case (INaddr)
             0: begin 
-                register0 = IN;
+                register0 <= IN;
             end
             1: begin 
-                register1 = IN;
+                register1 <= IN;
             end
             2: begin 
-                register2 = IN;
+                register2 <= IN;
             end
             3: begin 
-                register3 = IN;
+                register3 <= IN;
             end
             4: begin 
-                register4 = IN;
+                register4 <= IN;
             end
             5: begin 
-                register5 = IN;
+                register5 <= IN;
             end
             6: begin 
-                register6 = IN;
+                register6 <= IN;
             end
             7: begin 
-                register7 = IN;
+                register7 <= IN;
             end
         endcase // case (wrAddr)
         end
-    end // always @ (posedge clk)
+    end
 
+    // always @ (posedge clk)
     // Read functionality
     always @(posedge CLK ) begin
       if (CTRL==1) begin
         //OUT1
         case (OUT1addr)
           0: begin
-            OUT1 = register0;
+            OUT1 <= register0;
           end
           1: begin
-            OUT1= register1;
+            OUT1 <= register1;
           end
           2: begin
-            OUT1 = register2;
+            OUT1 <= register2;
           end
           3: begin
-            OUT1 = register3;
+            OUT1 <= register3;
           end
           4: begin
-            OUT1 = register4;
+            OUT1 <= register4;
           end
           5: begin
-            OUT1 = register5;
+            OUT1 <= register5;
           end
           6: begin
-            OUT1 = register6;
+            OUT1 <= register6;
           end
           7: begin
-            OUT1 = register7;
+            OUT1 <= register7;
           end
           default:
             OUT1 <= 0;
@@ -115,28 +158,28 @@ module regfile8x8a(IN, OUT1, OUT2, INaddr, OUT1addr, OUT2addr, CLK, RESET, CTRL)
         //OUT2
         case (OUT2addr)
           0: begin
-            OUT2 = register0;
+            OUT2 <= register0;
           end
           1: begin
-            OUT2 = register1;
+            OUT2 <= register1;
           end
           2: begin
-            OUT2 = register2;
+            OUT2 <= register2;
           end
           3: begin
-            OUT2 = register3;
+            OUT2 <= register3;
           end
           4: begin
-            OUT2 = register4;
+            OUT2 <= register4;
           end
           5: begin
-            OUT2 = register5;
+            OUT2 <= register5;
           end
           6: begin
-            OUT2 = register6;
+            OUT2 <= register6;
           end
           7: begin
-            OUT2 = register7;
+            OUT2 <= register7;
           end
           default:
             OUT2 <= 0;
