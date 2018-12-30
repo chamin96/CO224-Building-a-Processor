18d348aadb97ba8cc401862dd88b6516c72fbb3e
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2018-12-31 00:16:58 +0530

    mux testbench implemented

--- CPU.v
+++ CPU.v
@@ -31,5 +31,37 @@ module testbench_for_two_s;
     IN=8'b00000010;
     #1 $display ("2s complement of %b is %b.",IN,OUT);
   end
+endmodule // testbench_for_two_s
 
-endmodule // testbench
+module testbench_for_mux;
+  reg [7:0] IN1,IN2;
+  wire [7:0] OUT;
+  reg SELECT,CLK;
+
+  initial begin
+  //initial values
+    CLK=0;
+    SELECT=0;
+    IN1=5;
+    IN2=10;
+
+  //time=3
+  #3 $display ("OUT = %d",OUT);
+  //time=6
+  #6 $display ("OUT = %d",OUT);
+  //time=9
+  #9 SELECT=1;
+  #9 $display ("OUT = %d",OUT);
+  //time=12
+  #12 $display ("OUT = %d",OUT);
+  $finish;
+  end
+
+  // Clock generator
+  always begin
+    #5 CLK = ~CLK; // Toggle clock every 5 ticks
+  end
+
+  mux two_to_one_mux(IN1,IN2,SELECT,OUT,CLK);
+
+endmodule // testbench_for_mux
