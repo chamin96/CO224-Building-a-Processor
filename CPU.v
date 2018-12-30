84e73fc881a845a37a4eede97c247fb9a23cfb7d
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2018-12-30 23:56:58 +0530

    2:1 mux implemented

--- CPU.v
+++ CPU.v
@@ -6,7 +6,22 @@ module two_s_complement (IN,OUT);
 
 endmodule //two_s_complement
 
-module testbench;
+module mux (IN1,IN2,SELECT,OUT,CLK);
+  input [7:0] IN1,IN2;
+  input SELECT,CLK;
+  output reg [7:0] OUT;
+
+  always @ (negedge CLK) begin
+    case (SELECT)
+      0: OUT<=IN1;
+      1: OUT<=IN2;
+      default: OUT<=0;
+    endcase
+  end
+
+endmodule //mux 2 to 1
+
+module testbench_for_two_s;
   reg [7:0] IN;
   wire [7:0] OUT;
 
